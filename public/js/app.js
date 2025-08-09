46a12a1b178a1a017788650c2239248c917aace4
kozimovabduvali <kozimovabduvali2006@gmail.com>
2025-08-09 15:04:47 +0500

    захватывающий дух вид

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,11 @@ let swiper = new Swiper(".kazan-swipe__hero", {
   loop: true,
   spaceBetween: 20,
   autoplay: {
-    delay: 3000, 
+    delay: 3000,
     // disableOnInteraction: false,
   },
   breakpoints: {
-    640: { 
+    640: {
       slidesPerView: 1,
       spaceBetween: 20,
     },
@@ -14,5 +14,26 @@ let swiper = new Swiper(".kazan-swipe__hero", {
       slidesPerView: 1.5,
       spaceBetween: 20,
     }
+  },
+});
+
+
+let kazanSwiperMain = new Swiper('.kazan-swipe__main', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  centeredSlides: true,
+  // loop: true,
+  slideToClickedSlide: true,
+  autoplay: {
+    delay: 3000,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    init: function () {
+      this.slideToLoop(1, 0);
+    },
   },
 });
